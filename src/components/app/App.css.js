f08b1430f4ad6eb2b64f30aca1ea6cc259b5e2b4
Kristina Tong <kristina.tong@giantmachines.com>
2019-03-22 10:55:43 -0400

    Finalizing TypeScript and Eslint

--- src/components/app/App.css.js
+++ src/components/app/App.css.js
@@ -1,22 +1,22 @@
 import styled from 'styled-components';
-import * as Color from '../../styles/color';
-import * as Typography from '../../styles/typography';
+// import * as Color from '../../styles/color';
+// import * as Typography from '../../styles/typography';
 
 export const Wrapper = styled.div`
-    text-align: center;
+  text-align: center;
 `;
 
 export const Header = styled.header`
-    background-color: #282c34;
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    font-size: calc(10px + 2vmin);
-    color: white;
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
 `;
 
 export const Link = styled.a`
-    color: #61dafb;
+  color: #61dafb;
 `;
--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -4,25 +4,22 @@ import { GlobalStyle } from '../../styles/globalStyles';
 import { Wrapper, Header, Link } from './App.css';
 
 class App extends Component {
-  typeScriptFunction = (variable:string) => {
-
-  }
   render() {
     return (
       <Fragment>
         <GlobalStyle />
         <Wrapper>
           <Header>
-            <p>
-              Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <Link href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-              Learn React
-            </Link>
+            <p>Edit <code>src/App.tsx</code> and save to reload.</p>
+            <Link
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >Learn React</Link>
           </Header>
         </Wrapper>
       </Fragment>
-    )
+    );
   }
 }
 
